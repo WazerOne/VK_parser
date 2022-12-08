6b4b4f4df31c7ca4f70dca072792fc60e97d4b58
WazerOne <93553782+WazerOne@users.noreply.github.com>
2022-12-08 14:03:15 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -45,26 +45,6 @@ def get_subscribtions(user_id, n):
     return dataW
 
 
-
-# def parser_vk(links):
-#     with open('VK_parser_posts', 'w', encoding='utf-8') as file_posts:
-#         with open('VK_parser_subs', 'w', encoding='utf-8') as file_subs:
-#             a_pen = csv.writer(file_posts)
-#             a_pen.writerow((('user_id', 'id_post', 'text')))
-#             b_pen = csv.writer(file_subs)
-#             b_pen.writerow((('user_id', 'group_id', 'group_name')))
-#             for link in links:
-#                 time.sleep(0.5)
-#                 user_id = link.partition('id')[2]
-#                 TWpost = get_wallposts(user_id, 20)
-#                 TWsubs = get_subscribtions(user_id, 200)
-#                 for post in TWpost:
-#                     if post['text'] != '':
-#                         a_pen.writerow((user_id, post['id'], post['text']))
-#                 for sub in TWsubs:
-#                     if sub['type'] != 'profile':
-#                         b_pen.writerow((user_id, sub['id'], sub['name']))
-
 def parser_vk(links):
     df_posts = pd.DataFrame({'user_id': [],
                              'post_id': [],
@@ -97,45 +77,3 @@ with pd.ExcelWriter('C:/Users/wazer/PycharmProjects/vkparser/VK_parser.xlsx') as
 
 print(1)
 
-# def write_txt_post_to_csv(TWpost):
-#     with open('Lazitch_posts', 'w', encoding='utf-8') as file:
-#         a_pen = csv.writer(file)
-#         a_pen.writerow(('id_post', 'text'))
-#         for i in TWpost:
-#             a_pen.writerow((i['id'], i['text']))
-#
-#
-# def write_txt_subs_to_csv(TWsubs):
-#     with open('Lazitch_subs', 'w', encoding='utf-8') as file:
-#         a_pen = csv.writer(file)
-#         a_pen.writerow(('id', 'name'))
-#         for i in TWsubs:
-#             a_pen.writerow((i['id'], i['name']))
-#
-#
-#
-#
-# TWsubs = get_subscribtions(200)
-# write_txt_subs_to_csv(TWsubs)
-#
-# TWpost = get_wallposts(20)
-# write_txt_post_to_csv(TWpost)
-#
-# def parser_vk(links):
-#     with open('VK_parser_posts', 'w', encoding='utf-8') as file_posts:
-#         with open('VK_parser_subs', 'w', encoding='utf-8') as file_subs:
-#             a_pen = csv.writer(file_posts)
-#             a_pen.writerow((('user_id', 'id_post', 'text')))
-#             b_pen = csv.writer(file_subs)
-#             b_pen.writerow((('user_id', 'group_id', 'group_name')))
-#             for link in links:
-#                 time.sleep(0.5)
-#                 user_id = link.partition('id')[2]
-#                 TWpost = get_wallposts(user_id, 20)
-#                 TWsubs = get_subscribtions(user_id, 200)
-#                 for post in TWpost:
-#                     if post['text'] != '':
-#                         a_pen.writerow((user_id, post['id'], post['text']))
-#                 for sub in TWsubs:
-#                     if sub['type'] != 'profile':
-#                         b_pen.writerow((user_id, sub['id'], sub['name']))
